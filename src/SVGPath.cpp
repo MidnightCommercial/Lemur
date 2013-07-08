64434942b663737a9426baf3e60f62ed456eed44
Omer Shapira <info@omershapira.com>
2013-07-07 21:49:42 -0400

    Adds normalisation for SVG imports

--- src/SVGPath.cpp
+++ src/SVGPath.cpp
@@ -11,35 +11,25 @@
 
 void SVGPath::loadFile(string _filepath){
     filepath = _filepath;
-    //load an SVG
     ofxSVG svg;
     svg.load(filepath);
-    //convert stuff from Polylines to Polys
-    //dispose of SVG
-
+    float ratio = MAX(svg.getWidth(), svg.getHeight());
     for (int i = 0 ; i < svg.getNumPath(); i++){
         //TODO: use a different method of copying these once you get comfortable with this code (OmerShapira)
         //TODO: Now comes the point where you actually need to convert ofPath to ofPoly (OmerShapira)
-        ofPath path = svg.getPathAt(i); 
+        ofPath path = svg.getPathAt(i);
         //TODO: Make sure this isn't destructive. generatePolylinesFromPaths is not the only thing that happens there (OmerShapira)
         path.setPolyWindingMode(OF_POLY_WINDING_ODD);
         vector<ofPolyline>& polylines = path.getOutline();
         for (int j = 0; j < polylines.size(); j++) {
-            //TODO: Check if this isn't returning a pointer (OmerShapira)
             ofxIlda::Poly p1;
             vector<ofVec3f> pts = polylines[j].getVertices();
             for(int i = 0; i < pts.size(); i++){
-                p1.addVertex(ofPoint(pts[i].x/svg.getWidth(), pts[i].y/svg.getHeight()));
+                p1.addVertex(ofPoint(pts[i].x/ratio, pts[i].y/ratio));
             }
-        
             p1 = p1.getResampledByCount(1500);
             p1.color = svg.getPathAt(i).getFillColor();
             polys.push_back(p1);
-            //cout<<"path "<<i<<": "<<polylines[i].getVertices().size()<<endl;
-//            for (int w = 0 ; w<polylines[i].size();  ++w){
-//                cout<<ofToString(polylines[i][j])<<", ";
-//            }
-//            cout<<endl;
         }
     }
     //TODO: Check if the file is really disposed now - we're not supposed to need it anymore (OmerShapira)
