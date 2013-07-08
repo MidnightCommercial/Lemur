00920e0a5bd5ef3fc898454c31f55110a699c6fb
Omer Shapira <info@omershapira.com>
2013-07-08 01:43:31 -0400

    adds Property<T>

---
+++ src/LemurUtils.h
@@ -0,0 +1,51 @@
+//
+//  LemurUtils.h
+//  Lemur
+//
+//  Created by Omer Shapira on 08/07/13.
+//
+//
+#pragma once
+
+namespace Lemur {
+    
+    template<typename T>
+    class Property{
+        T _value;
+        string _name;
+    public:
+        Property(string name="Unnamed Property"): _name(name){}
+//        ~Property(){}
+        
+        T operator()(){ return _value; }
+        T & operator=(const T &rhs){
+            _value = rhs;
+        }
+        
+                string getString(){
+                    //TODO: + is probably unsafe
+                    ostringstream output;
+                    output<<ofToString(_name)<<" : "<<ofToString(_value);
+                    return output.str();
+                }
+        
+        //TODO: Complete operator overloading
+        friend ostream& operator<<(ostream&, const Lemur::Property<T>&);
+    };
+    
+}
+
+//template<typename T>
+//ostream& operator<<(ostream& strm, const Lemur::Property<T>& t) {
+//    strm << t._name <<" : "<< t.value;
+//    return strm;
+//    }
+
+    
+    namespace Lemur{
+        typedef Property<float> LFloat;
+        typedef Property<int>   LInt;
+        typedef Property<string> LString;
+    }
+
+    
